70da7508a2807da9b85cb4639ab1b60f65be2558
Judy_eunji <dmswlckato@gmail.com>
2021-12-21 10:00:29 +0900

    feat: adapting and extending

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
+import styled from "styled-components";
+
 function App () {
 
-  return null;
+  return (
+    <Container>
+      <Box bgColor={"teal"}>
+        <Text>Hello</Text>
+      </Box>
+      <Circle bgColor={"tomato"}>
+        <Text>
+          Hi
+        </Text>
+      </Circle>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const Box = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+const Text = styled.span`
+  color: white;
+  font-size: 16px;
+  font-weight: bold;
+`;
 
 export default App;
