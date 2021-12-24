c0c61f0ebb87a3ba6bc740f5931037546283f2c7
Judy_eunji <dmswlckato@gmail.com>
2021-12-24 21:51:14 +0900

    feat: forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import Circle from './Circle';
+import React, { useState } from "react";
+import {css} from "styled-components";
+// import Circle from './Circle';
 
 function App () {
 
+  const [value, setValue] = useState("")
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    console.log(e.currentTarget.value);
+    const { currentTarget: {value} } = e;
+    setValue(value)
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("hello", value);
+  };
+
   return (
     <div>
-      <Circle bgColor={"teal"} borderColor={"yellow"} />
-      <Circle text={"Title"} bgColor={"tomato"} />
+      <form onSubmit={onSubmit}>
+        <input value={value}
+               onChange={onChange}
+               type="text"
+               placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
+      {/*<Circle bgColor={"teal"} borderColor={"yellow"} />*/}
+      {/*<Circle text={"Title"} bgColor={"tomato"} />*/}
     </div>
   );
 }
