1317031f3f4a07031a8bafe6ae05657b8fadbee1
shin-eunji <dmswlckato@gmail.com>
2022-01-09 18:05:28 +0900

    feat: apexcharts library

--- src/routes/chart.tsx
+++ src/routes/chart.tsx
@@ -1,24 +1,77 @@
 import {useQuery} from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
 
 interface CharProps {
     coinId: string
 }
 
+interface InterfaceHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
 function Chart ({ coinId }: CharProps) {
 
-    const { isLoading, data } = useQuery(["ohlcv", coinId], () => fetchCoinHistory(coinId));
+    const { isLoading, data } = useQuery<InterfaceHistorical[]>(["ohlcv", coinId], () => fetchCoinHistory(coinId));
 
     return (
-      <>
+      <div>
       {
           isLoading ? (
-            "Loading..."
+            "Loading Chart..."
             ) : (
-              <div>{coinId}</div>
+            <ApexChart type={"line"}
+                       series={[
+                         {
+                           name: "Price",
+                           data: data?.map(price => price.close),
+                         },
+                       ]}
+                       options={{
+                         theme: {
+                           mode: "dark"
+                         },
+                         chart: {
+                           height: 200,
+                           width: 500,
+                           toolbar: {
+                             show: false
+                           },
+                           background: "transparent",
+                         },
+                         grid: {
+                           show: false,
+                         },
+                         stroke: {
+                           curve: "smooth",
+                           width: 3,
+                         },
+                         yaxis: {
+                           show: false,
+                         },
+                         xaxis: {
+                           axisBorder: {
+                             show: false,
+                           },
+                           axisTicks: {
+                             show: false,
+                           },
+                           labels: {
+                             show: false
+                           }
+                         }
+                       }}
+            />
           )
       }
-      </>
+      </div>
     )
 }
 
