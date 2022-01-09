d22b0748f84562a94cac8e9736e418a13fc6a1ad
shin-eunji <dmswlckato@gmail.com>
2022-01-09 15:07:13 +0900

    feat: data typescript

--- src/routes/coin.tsx
+++ src/routes/coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams, useLocation } from "react-router";
 import styled from "styled-components";
 
@@ -28,12 +28,89 @@ interface RouteParams {
 interface RouteState {
   name: string;
 }
+// interface Itag {
+//   coin_counter: number;
+//   ico_counter: number;
+//   id: string;
+//   name: string;
+// }
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  // tags: Itag[];
+  team: object;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface priceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: number;
+      ath_price: number;
+      market_cp: number;
+      market_cap_change__24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    }
+  };
+
+}
+
 
 function Coin() {
   const [loading, setLoading] = useState(true);
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<priceData>();
   const { coinId } = useParams<RouteParams>();
   const { state } = useLocation<RouteState>();
-  console.log("state.name", state.name);
+
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+      setLoading(false)
+    })();
+  }, [])
 
   return (
     <Container>
@@ -43,7 +120,10 @@ function Coin() {
       {loading ? (
         <Loader>Loading...</Loader>
       ) : (
-        null
+        <>
+          <span>{info.id}</span>
+          <span>{priceInfo?.quotes?.USD?.price}</span>
+        </>
       )
       }
     </Container>
